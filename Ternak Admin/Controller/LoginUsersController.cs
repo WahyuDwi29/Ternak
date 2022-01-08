70ec825079c295124b22cff253898465c1abad05
Wahyu Dwe <wahyu5161.w5@gmail.com>
2022-01-08 15:36:29 +0700

    add confirm on login

--- Ternak Admin/Controller/LoginUsersController.cs
+++ Ternak Admin/Controller/LoginUsersController.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
 using Ternak_Admin.Model;
 using Ternak_Admin.View;
 
@@ -25,13 +27,26 @@ namespace Ternak_Admin.Controller
             if (result)
             {
                 var main = new MainWindow();
+                var confirm = new ConfirmWindow();
+                confirm.ImgIcon.Source = new BitmapImage(new Uri("/Img/ic_check_circle.png", UriKind.Relative));
+                confirm.Lbl1.Content = "Login Berhasil";
+                confirm.Lbl2.Content = "";
+                confirm.Lbl3.Content = "";
+                confirm.Height = 125;
+                confirm.Width = 340;
+                confirm.ShowDialog();
                 main.LblUser.Content = UsersModel.UserName;
                 main.Show();
                 _loginWindow.Close();
             }
             else
             {
-                MessageBox.Show("Your username/password is wrong");
+                var confirm = new ConfirmWindow();
+                confirm.ImgIcon.Source = new BitmapImage(new Uri("/Img/ic_cancel.png", UriKind.Relative));
+                confirm.Lbl1.Content = "Login Gagal";
+                confirm.Lbl2.Content = "pastikan anda memasukan data dengan benar";
+                confirm.Lbl3.Content = "";
+                confirm.ShowDialog();
                 _loginWindow.txtUsername.Text = "";
                 _loginWindow.txtPassword.Password = "";
                 _loginWindow.txtUsername.Focus();
